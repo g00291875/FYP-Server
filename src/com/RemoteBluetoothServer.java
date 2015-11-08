f3a815afde3bc0be1eb947fe5eb9460a0278d189
g00291875 <g00291875@gmit.ie>
2015-11-08 21:37:16 +0000

    C1

---
+++ src/com/RemoteBluetoothServer.java
@@ -0,0 +1,9 @@
+package com;
+
+public class RemoteBluetoothServer{
+
+    public static void main(String[] args) {
+        Thread waitThread = new Thread(new WaitThread());
+        waitThread.start();
+    }
+}
