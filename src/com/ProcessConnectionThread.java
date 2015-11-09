8179ca6ad0045fb8cf99dab13b78d81d995dfe97
g00291875 <g00291875@gmit.ie>
2015-11-09 17:57:11 +0000

    multi thread server receiving strings

--- src/com/ProcessConnectionThread.java
+++ src/com/ProcessConnectionThread.java
@@ -4,7 +4,9 @@ import java.awt.event.KeyEvent;
 
         import java.awt.Robot;
         import java.awt.event.KeyEvent;
-        import java.io.InputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStream;
 
         import javax.microedition.io.StreamConnection;
 
@@ -12,10 +14,13 @@ public class ProcessConnectionThread implements Runnable {
 
     private StreamConnection mConnection;
 
+
     // Constant that indicate command from devices
     private static final int EXIT_CMD = -1;
     private static final int KEY_RIGHT = 1;
     private static final int KEY_LEFT = 2;
+    private DataInputStream dataInputStream;
+    private DataOutputStream dataOutputStream;
 
     public ProcessConnectionThread(StreamConnection connection)
     {
@@ -26,22 +31,33 @@ public class ProcessConnectionThread implements Runnable {
     public void run() {
         try {
             // prepare to receive data
-            InputStream inputStream = mConnection.openInputStream();
+
+
+            //InputStream inputStream = mConnection.openInputStream();
+
+            //dataInputStream = new DataInputStream(mConnection.getInputStream());
+           // dataOutputStream = new DataOutputStream(mConnection.getOutputStream());
+
+            dataInputStream = new DataInputStream(mConnection.openInputStream());
+            dataOutputStream = new DataOutputStream(mConnection.openOutputStream());
+
 
             System.out.println("waiting for input");
 
             while (true) {
-                int command = inputStream.read();
-
-                if (command == EXIT_CMD) {
-                    System.out.println("finish process");
-                    break;
+                if(dataInputStream.available() > 0){
+                    byte[] msg = new byte[dataInputStream.available()];
+                    dataInputStream.read(msg, 0, dataInputStream.available());
+                    System.out.print(new String(msg)+"\n");
+                    //sendHandler(ChatActivity.MSG_BLUETOOTH, nameBluetooth + ": " + new String(msg));
                 }
-                processCommand(command);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        /**
+         */
     }
 
     /**
